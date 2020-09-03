9ee86e277e5911ad50f862a616c5daf6aa7d7ed4
ben-kaye <benkaye001@gmail.com>
2020-09-03 18:54:00 +0100

    improve

--- MPC_QPs/mpc_ca_fixed_obj.m
+++ MPC_QPs/mpc_ca_fixed_obj.m
@@ -7,30 +7,29 @@
 % * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 % *                                                                     *
 % *             CrazyFlie controller model by Aren Karapet              *
-% *                          Using OSQP Solver                          *
 % *                                                                     *
 % * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-clear, clc
+clear,clc
 
-% PARAMETERS
+opts =  optimset('Display','off');
+
+%%% PARAMETERS %%%
+
 VIS_ON = 1;
-N = 15;
-N_OBJ_STATES = 2;
-MIN_DIST = 0.05;
-IDEAL_DIST = 0.9;
-EPSILON = [ 0.1 0.5 1 ];
-SIM_TIME = 30;
+Np = 5; % predictive horizon
+MIN_DIST = 0.05; % {m} 
+IDEAL_DIST = 0.7; % {m} radius of avoidance
+EPSILON = [ 0.1 0.5 1 ]; % init sequence ( dist = epsilon * delta )
+SIM_TIME = 30; % {s}
 
-% initial conditions
+%%% INITIAL CONDITIONS %%% 
+
 ref = [ 2; 0.5; 0; 0; 0; 0 ];
 x0 = [ 0.1; 0.1; 0; 0; 0; 0 ];
-obj_xy = [ 1; 0.5 ];
+p_o = [ 1; 0.5 ];
 
-% System State-Space
-
-% Precomputed system with Ts = 0.1. Control inputs desired x vel and y vel.
-
+% Hard coded state transition. Obtained in formulate_state_space.m
 Ad = [   1,  0,  0.0962912484723868,     0,                      0,                      0.0396218015830554;
         0,  1,  0,                      0.0962912484723868,     -0.0396218015830554,    0;
         0,  0,  0.894293322749997,      0,                      0,                      0.702694931894877;
@@ -39,87 +38,62 @@ Ad = [   1,  0,  0.0962912484723868,     0,                      0,             
         0,  0,  -0.193245482770890,     0,                      0,                      0.452393730892869   ];
 
 Bd = [   0.00370875152761323,    0;
-        0,                      0.00370875152761323;
-        0.105706677250003,      0;
-        0,                      0.105706677250003;
-        0,                      -0.193245482770890;
-        0.193245482770890,      0                       ];
-
+    0,                      0.00370875152761323;
+    0.105706677250003,      0;
+    0,                      0.105706677250003;
+    0,                      -0.193245482770890;
+    0.193245482770890,      0                       ];
     
 [nx, nu] = size(Bd);     
-% augmented state no.
-Nstates = (N+1)*nx + N*nu + N_OBJ_STATES;    
-    
-   
-    
 
+%%% CONSTRAINTS %%%
 
-% predictive horizon
-
-
-% constraints
 umin = [ -1; -1 ];
 umax = [ 1; 1 ];
 xmin = [ -inf; -inf; -2; -2; -inf; -inf ];
 xmax = [ inf; inf; +2; +2; inf; inf ];
 
-
-% LQ objective
+%%% OBJECTIVES %%%
 Q = diag([1 1 0.1 0.1 0 0]);
 QN = Q;
 R = 0.01 * eye(nu);
 
-% Augmented Quadratic Objective
-P = blkdiag( kron(speye(N), Q ), QN, zeros(N_OBJ_STATES), kron(speye(N), R));
-% Augmented Linear Objective
-q = [ repmat( -Q*ref, N, 1 ); -QN*ref; zeros(N_OBJ_STATES,1); zeros(N*nu, 1)];
-% q = zeros( nx*(N+1) + N*nu, 1 );
+% quadratic objective
+P = blkdiag( kron(speye(Np), Q ), QN, kron(speye(Np), R));
+% linear objective
+q = [ repmat( -Q*ref, Np, 1 ); -QN*ref; zeros(Np*nu, 1)];
 
 
-% Augmented Dynamics
-Ax = kron( speye(N+1), -speye(nx) ) +...
-    kron( sparse(diag(ones(N,1), -1)), Ad);
-Bu = kron( [ sparse(1, N); speye(N) ], Bd );
-
-% append dynamics of object = stationary
-Ax = blkdiag(Ax, -speye(N_OBJ_STATES));
-Bu = [Bu; zeros(N_OBJ_STATES, size(Bu, 2))];
+% constrain predicted system to dynamic model
+Ax = kron( speye(Np+1), -speye(nx) ) +...
+    kron( sparse(diag(ones(Np,1), -1)), Ad);
+Bu = kron( [ sparse(1, Np); speye(Np) ], Bd );
 
 Aeq = [ Ax, Bu ];
 
-leq = [ -x0; zeros(N*nx, 1); -obj_xy ];
+leq = [ -x0; zeros(Np*nx, 1) ];
 ueq = leq;
-xN = leq;
-
 
 % input and state constraints
 
-Aineq = speye( Nstates );
+Aineq = speye((Np+1)*nx + Np*nu);
 
-lineq = [ repmat( xmin, N+1, 1); -inf*ones(N_OBJ_STATES, 1); repmat( umin, N, 1)  ];
-uineq = [ repmat( xmax, N+1, 1); +inf*ones(N_OBJ_STATES, 1); repmat( umax, N, 1) ];
+lineq = [ repmat( xmin, Np+1, 1); repmat( umin, Np, 1)  ];
+uineq = [ repmat( xmax, Np+1, 1); repmat( umax, Np, 1) ];
 
-Aconstr = [ kron(speye(N+1), [ 1 1 0 0 0 0]), -ones(N+1, N_OBJ_STATES), zeros(N+1, N*nu) ];
-
-% osqp constraints
-A = [ Aeq; Aineq; Aconstr ];
-l = [ leq; lineq; -inf*ones((N+1), 1) ];
-u = [ ueq; uineq; inf*ones((N+1), 1)];
-
-
-% osqp init and setup
-prob = osqp;
-prob.setup( P, q, A, l, u, 'warm_start', true, 'verbose', false );
+[ A_dist, ~ ] = dist_constraints(Np, nx, nu, p_o, zeros(Np, 1), -leq);
+l_dist = -inf*ones(Np, 1);
+u_dist = inf*ones(Np,1);
 
 % simulate
 
 if (VIS_ON)
     figure(1)
-    plot(x0(1), x0(2), 'bx', 'MarkerSize', 30, 'DisplayName', 'Start')
+    plot(x0(1), x0(2), 'b+', 'MarkerSize', 30, 'DisplayName', 'Start')
     hold on
     plot(ref(1), ref(2), 'rx', 'MarkerSize', 30, 'DisplayName', 'End')
-    plot(obj_xy(1), obj_xy(2), 'r+', 'DisplayName', 'Radius of Avoidance')
-    viscircles(obj_xy', IDEAL_DIST)
+    plot(p_o(1), p_o(2), 'r+', 'DisplayName', 'Radius of Avoidance')
+    viscircles(p_o', IDEAL_DIST, 'LineStyle', '-.')
 
     xlabel('x (m)')
     ylabel('y (m)')
@@ -128,41 +102,39 @@ end
 
 error_count = 0;
 
-x_hist = zeros((N+1)*nx, SIM_TIME);
+x_hist = NaN*ones((Np+1)*nx, SIM_TIME);
 
-% solve unconstrained problem
-res = prob.solve();
-xN = res.x;
 
+% initialisation sequence for dist constraints
+xN = quadprog(P, q, A_dist, u_dist, Aeq, ueq, lineq, uineq, [], opts);
 for e = EPSILON
     
-    relaxation_params = e * ones(N+1, 1);
+    relaxation_params = e * ones(Np, 1);
     
-    dists = MIN_DIST * ( ones(N+1,1) - relaxation_params ) + IDEAL_DIST * relaxation_params;
+    dists = MIN_DIST * ( ones(Np,1) - relaxation_params ) + IDEAL_DIST * relaxation_params;
     
-    [ A, l ] = compute_linear_constrs(A, l, N, nx, nu, N_OBJ_STATES, dists, xN);
-    
-    prob.update('l',l, 'Ax', nonzeros(A));
-    
-    res = prob.solve();
-    xN = res.x; 
+    [ A_dist, u_dist ] = dist_constraints(Np, nx, nu, p_o, dists, xN);
+
+    xN = quadprog(P, q, A_dist, u_dist, Aeq, ueq, lineq, uineq, [], opts);  
 end
 
+%%% SIMULATE %%%
 
 for i = 1 : SIM_TIME
     
-    res = prob.solve();
-    % record result if solved
-    if ~strcmp(res.info.status, 'solved')
-            error_count = error_count + 1;                
-    else 
-        error_count = 0;
-        xN = res.x;
-    end 
-   
+    x_result = quadprog(P, q, A_dist, u_dist, Aeq, ueq, lineq, uineq, [], opts);
+    
+    if sum(isnan(x_result)) > 0
+        % err
+        error_count = error_count + 1;
+    else
+        % solved
+        xN = x_result;
+    end
+    
     % get control actions
-    ctrls = xN( (N+1)*nx + N_OBJ_STATES + 1 : end );
-    ctrls = reshape(ctrls, [nu, N]);
+    ctrls = xN( (Np+1)*nx + 1 : end );
+    ctrls = reshape(ctrls, nu, []);
     
     % in event of unsolvable path, carry on with previously calculated
     % optimal path
@@ -175,60 +147,31 @@ for i = 1 : SIM_TIME
     x_hist(1:nx,i) = x0;
     
     % compute linear constraints
-    [ A, l ] = compute_linear_constrs(A, l, N, nx, nu, N_OBJ_STATES, dists, xN);
-    
-    % update initial conds and constraints
-    l(1:nx) = -x0;
-    u(1:nx) = -x0;
-    
-    prob.update('l',l, 'u',u);
-    prob.update('Ax', nonzeros(A));
-  
-  
+    [ A_dist, u_dist ] = dist_constraints(Np, nx, nu, p_o, dists, xN);
+        
+    leq(1:nx) = -x0;
+    ueq(1:nx) = -x0; 
 end
 
 % plot results
 if (VIS_ON)
     plot(x_hist(1,:), x_hist(2,:), '-^', 'LineWidth', 1.5, 'MarkerSize', 5, 'Color', 1/255*[73, 146, 214], 'DisplayName', 'Path Taken')
     axis equal
-    legend()
+    legend();
 end
 
-function [ A, l ] = compute_linear_constrs(A, l, N, nx, nu, no, min_dists, x)
-    [ m, n ] = size(A);
-    start_idx = m - N - 1; % i = start_idx + k
+function [ A_dist, u_dist ] = dist_constraints(Np, nx, nu, p_o, min_dists, x_mpc)
+    x_mpc = x_mpc(1:(Np+1)*nx);
+    x_mpc = reshape(x_mpc, nx, []);
 
-    % get obj coords
-    obj_xy = x((N+1)*nx + 1:(N+1)*nx + no);
-
-    % assign dists to <= cstr
-    l(end-N:end) = min_dists;
-
-    % for each time step
-    for k = 1:N+1
-        % get state at k
-        state_k = x( (k-1)*nx + 1 : k*nx );
-
-        % get vec to obj
-        x_ij = state_k(1:2) - obj_xy;
-        x_norm = norm(x_ij,2);
-
-        eta = x_ij / x_norm;    
-
-        T = eta' * [ eye(no), -eye(no) ];
-
-        % assign to A where Ax>=l
-        A( start_idx + k,...
-            [ (k-1)*nx + 1 : (k-1)*nx + 2, (N+1)*nx + 1 : (N+1)*nx + no ] )...
-            = T;
-    end
+    % past pos at k
+    p_k = x_mpc([1,2], :);
+    
+    p_ox = p_k(:, 2:end) - p_o;
+    eta = p_ox./vecnorm(p_ox);
+    
+    u_dist = -min_dists - eta'*p_o;
+    
+    A_dist = [ zeros(Np, nx), kron(speye(Np), [ 1, 1, zeros(1, nx - 2)]), zeros(Np, nu*Np) ];
+    A_dist(A_dist ~= 0) = -eta;    
 end
-
-
-
-
-
-
-
-
-
